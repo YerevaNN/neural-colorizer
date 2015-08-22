a9112fc0f34b8cd6f64477fe133abdf16d64a988
Hrayr Harutyunyan <harhro@gmail.com>
2015-08-22 14:58:27 +0400

    Fixed learning rate bug, added learning rate policy

--- train_convae.py
+++ train_convae.py
@@ -19,7 +19,6 @@ definition = imp.load_source('definition', 'definitions/%s.py' % network_name)
 
 def test_convae(batch_size = 100, 
                 n_epochs = 10000,
-                learning_rate = 0.01,
                 save_iter = 50,
                 info_iter = 20,
                 val_iter = 10,
@@ -54,38 +53,80 @@ def test_convae(batch_size = 100,
     
 
     # build network
-    network = definition.define_model(input_var)
-    target_var = layers.get_output(network)
+    # target_var is output of validation network and is defined in definition file
+    # output for train network is always layers.get_output(network)
+    # these two are seperated, because train and val networks may differ from each other
+    network, target_var = definition.define_model(input_var)
+    
+    
+    # learning rate, constant 0.001 if not specified
+    if (hasattr(definition, 'lr_policy')):
+        lr_policy = definition.lr_policy
+        if (lr_policy == 'constant'):
+            lr_base = definition.lr_base
+        elif (lr_policy == 'geom'):
+            lr_base = definition.lr_base
+            lr_step = definition.lr_step
+            lr_coef = definition.lr_coef
+        else:
+            raise Exception("Unknown lr_policy\nSupported: 'constant', 'geom'\n")
+    else:
+        lr_policy = 'constant'
+        lr_base = 0.001
+    
+    learning_rate = theano.shared(np.float32(lr_base))
+        
+    print "==> lr_policy = %s" % lr_policy 
+    if (lr_policy == 'constant'):
+        print "\tbase_lr = %f" % lr_base
+        lr_desc = "-lr_c%f" % lr_base
+    elif (lr_policy == 'geom'):
+        print "\tlr_base = %f" % lr_base
+        print "\tlr_step = %d" % lr_step
+        print "\tlr_coef = %f" % lr_coef
+        lr_desc = "-lr_g_%f_%d_%f" % (lr_base, lr_step, lr_coef)
 
     
-    # some functions
-    cost, updates = definition.get_cost_updates(
+    # functions for train network
+    train_cost, train_updates = definition.get_cost_updates(
         network = network,
         input_var = input_var,
+        output = layers.get_output(network),
         learning_rate = learning_rate,
     )
 
     train = theano.function(
         [index],
-        cost,
-        updates = updates,
+        train_cost,
+        updates = train_updates,
         givens = {
             input_var: train_set_x[index * batch_size: (index + 1) * batch_size]
         }
     )
     
+    
+    # functions for validation network
+    # val_updates is a dummy variable 
+    forward = theano.function(
+        [input_var], 
+        target_var,
+    )
+    
+    val_cost, val_updates = definition.get_cost_updates(
+        network = network,
+        input_var = input_var,
+        output = target_var,
+        learning_rate = learning_rate,
+    )
+    
     validation = theano.function(
         [index],
-        cost,
+        val_cost,
         givens = {
             input_var: val_set_x[index * batch_size: (index + 1) * batch_size]
         }
     )
     
-    forward = theano.function(
-        [input_var], 
-        target_var,
-    )
     
     # loading model
     if load_model_name == '':
@@ -101,81 +142,107 @@ def test_convae(batch_size = 100,
 
 
     # model_name
-    model_name = network_name + "-lrcoef0.8" + "-lr" + str(learning_rate)
+    model_name = network_name + lr_desc
+    if (len(sys.argv) >= 3):
+        # to allow run temporary models not changing log files
+        model_name = sys.argv[2]
     print "==> network_name = %s" % network_name
     print "==> model_name = %s" % model_name
     
-    
+   
     # create log files
     if (not os.path.exists(log_dir)):
         os.makedirs(log_dir)
     
     train_loss_file = open(log_dir + '/log_train_' + model_name + '.csv', 'w')
     val_losss_file = open(log_dir + '/log_val_' + model_name + '.csv', 'w')
-    
-    
+   
+   
     # train     
     print "==> Training has started"
     start_time = time.time()
-    for epoch in xrange(start_epoch, n_epochs):
-         
-        costs = []
-        for batch_index in xrange(n_train_batches):
-            costs.append(train(batch_index))
 
-        print >> train_loss_file, "%d, %f" % (epoch, np.mean(costs))        
-        train_loss_file.flush()
-        print "Training epoch %d took %.0fs, lr=%f, loss=%f" % (epoch, time.time() - start_time, learning_rate, np.mean(costs))
-
-        if (epoch == 5):
-            learning_rate *= 10
-
-        if (epoch % 20 == 1):
-                learning_rate = learning_rate * 0.8
-        
-        # validation
-        if (epoch % val_iter == 1):
+    try:
+        for epoch in xrange(start_epoch, n_epochs):
+             
             costs = []
-            for batch_index in xrange(n_val_batches):
-                costs.append(validation(batch_index))
+            for batch_index in xrange(n_train_batches):
+                costs.append(train(batch_index))
+    
+            print >> train_loss_file, "%d, %f" % (epoch, np.mean(costs))        
+            train_loss_file.flush()
+            print "Training epoch %d took %.0fs, lr=%f, loss=%f" % (epoch, time.time() - start_time, learning_rate.eval(), np.mean(costs))
+    
+    
+            # validation
+            if (epoch % val_iter == 1):
+                costs = []
+                for batch_index in xrange(n_val_batches):
+                    costs.append(validation(batch_index))
+                
+                print >> val_losss_file, "%d %f" % (epoch, np.mean(costs))
+                val_losss_file.flush()
+                print "==> Validation loss = %f" % np.mean(costs)
+    
+    
+            # learning rate policy
+            if (lr_policy == 'geom'):
+                if (epoch > start_epoch + 1 and epoch % lr_step == 1):
+                    learning_rate *= lr_coef
+
+    
+            # save
+            if (epoch % save_iter == 1 and epoch > start_epoch + 1):
+                our_utils.save_model(
+                    network = network,
+                    epoch = epoch,
+                    model_name = model_name,
+                    learning_rate = learning_rate.eval(),
+                    directory = save_dir,
+                )
+                
+        
+            # info        
+            if (epoch % info_iter == 1):
+                our_utils.print_samples(
+                    images = T.concatenate([train_set_x[0:75], val_set_x[0:75]], axis = 0),
+                    forward = forward, 
+                    model_name = model_name,
+                    epoch = epoch,
+                    columns = 5,
+                    directory = info_dir,
+                )
+                our_utils.plot_filters(
+                    filters = layers.get_all_param_values(network)[0],
+                    model_name = model_name,
+                    epoch = epoch,
+                    repeat = 10,
+                    columns = 8,
+                    directory = info_dir,
+                )
             
-            print >> val_losss_file, "%d %f" % (epoch, np.mean(costs))
-            val_losss_file.flush()
-            print "==> Validation loss = %f" % np.mean(costs)
+            start_time = time.time()
 
-
-        # save
-        if (epoch % save_iter == 1 and epoch > start_epoch + 1):
+    except KeyboardInterrupt:
+        answer = raw_input("\nWould you like to save the model ? (y / n) ")
+        if (answer in ["y", "Y", "yes"]):
             our_utils.save_model(
                 network = network,
                 epoch = epoch,
                 model_name = model_name,
-                learning_rate = learning_rate,
+                learning_rate = learning_rate.eval(),
                 directory = save_dir,
-            )
-            
-    
-        # info        
-        if (epoch % info_iter == 1):
-            our_utils.print_samples(
-                images = T.concatenate([train_set_x[0:75], val_set_x[0:75]], axis = 0),
-                forward = forward, 
-                model_name = model_name,
-                epoch = epoch,
-                columns = 5,
-                directory = info_dir,
-            )
-            
+            )    
         
-        start_time = time.time()
+        exit()
+
 
 if __name__ == '__main__':
     if (len(sys.argv) <= 1):
-        sys.exit("Usage: convae.py <definition_file_name>")
+        sys.exit("Usage: convae.py <definition_file_name> [<model_name>]")
     
     test_convae(
         batch_size = 100,
-        learning_rate = 0.0002,
         load_model_name = '',
         info_iter = 20,
         val_iter = 10,
